06d56f6283248228158aeec8ebffadb1127c50fe
darkkkkkknight <1520783971@qq.com>
2021-07-11 17:52:39 +0800

    尝试使用hashRouter

--- src/components/ResultHeader/index.jsx
+++ src/components/ResultHeader/index.jsx
@@ -7,7 +7,7 @@ function ResultHeader(props) {
     return (
         <div style={{backgroundColor:'rgb(55,133,140)', display:'flex', alignItems:'center'}}>
             <Link to={"/index"}>
-                <img style={{width:'10rem',height:'3.5rem',paddingLeft:'.5rem'}} src={logo} alt="logo"/>
+                <img style={{width:'10rem',paddingLeft:'.5rem'}} src={logo} alt="logo"/>
             </Link>
             <div className={'search-bar-container'} style={{width:'20rem',height:'2.5rem'}}>
                 <input id={'searchInput'}  type="text" placeholder="请输入搜索内容..."/>
