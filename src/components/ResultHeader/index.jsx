3abcd490530513ec0a4e736a53dae220f520a339
73-SEVENTYTHREE <2569535507@qq.com>
2021-07-11 16:22:35 +0800

    解决导航栏图片间距问题

--- src/components/ResultHeader/index.jsx
+++ src/components/ResultHeader/index.jsx
@@ -5,16 +5,15 @@ import './index.css';
 
 function ResultHeader(props) {
     return (
-        <div style={{backgroundColor:'rgb(55,133,140)', display:'flex', alignItems:'center'}}>
+        <div style={{backgroundColor:'rgb(55,133,140)', display:'flex', alignItems:'center', height:'4rem'}}>
             <Link to={"/index"}>
-                <img style={{width:'10rem',height:'3.5rem',paddingLeft:'.5rem'}} src={logo} alt="logo"/>
+                <img style={{width:'10rem',paddingLeft:'.6rem'}} src={logo} alt="logo"/>
             </Link>
-            <div className={'search-bar-container'} style={{width:'20rem',height:'2.5rem'}}>
+            <div className={'search-bar-container'} style={{width:'35rem',height:'2.5rem'}}>
                 <input id={'searchInput'}  type="text" placeholder="请输入搜索内容..."/>
                 <button id={'searchButton'} onClick={()=>{ window.location.href = '/result' }}/>
             </div>
         </div>
     );
 }
-
 export default ResultHeader;
