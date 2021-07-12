6800dc54b119caa6aa93df6e81942e64cf3436b8
73-SEVENTYTHREE <2569535507@qq.com>
2021-07-12 09:28:43 +0800

    尝试解决TypeTag布局错乱问题

--- src/components/TypeTag/index.jsx
+++ src/components/TypeTag/index.jsx
@@ -5,7 +5,7 @@ import {Tag} from "antd";
 function TypeTag (props) {
     const type = props.type;
     return (
-        <Tag icon={<img src={TV} alt={'tv'} style={{width:'.6rem'}}/>}
+        <Tag icon={<img src={TV} alt={'tv'} style={{display:'inline-block', width:'.6rem'}}/>}
              style={{height:'1.4rem', width:'3.5rem', display:'flex', alignItems:'center', justifyContent:'center'}}
         >
             &nbsp;番剧
