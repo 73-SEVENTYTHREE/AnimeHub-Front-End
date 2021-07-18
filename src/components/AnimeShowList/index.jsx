be15160cb6533de164227fc40e6134245b92046b
darkkkkkknight <1520783971@qq.com>
2021-07-18 13:15:14 +0800

    完善搜索结果页面

--- src/components/AnimeShowList/index.jsx
+++ src/components/AnimeShowList/index.jsx
@@ -5,6 +5,7 @@ import {Link} from "react-router-dom";
 import TypeTag from "../TypeTag";
 import moment from 'moment';
 import BiliBiliScoreTag from "../BiliBiliScoreTag";
+import BangumiScoreTag from "../BangumiScoreTag";
 import './index.css';
 
 const { CheckableTag } = Tag;
@@ -90,20 +91,30 @@ function AnimeShowList(props) {
                                         </Link>&nbsp;&nbsp;
                                         <TypeTag type={item.type}/>
                                     </div>}
-                                    description={item.tags.map(item=>{return <Tag><div dangerouslySetInnerHTML={item}/></Tag>})}
+                                    description={item.tags.map((item,index)=>{
+                                        if(index<14){
+                                            return <Tag><div dangerouslySetInnerHTML={item}/></Tag>
+                                        }
+                                    })}
                                 />
+                                <div className={'item-info-tag'}><Tag color={'geekblue'}>原名</Tag>{item.primary_name || '暂无'}</div>
                                 <div className={'item-info-tag'}>
-                                    <Tag color={'geekblue'}>评分:</Tag>
+                                    <Tag color={'geekblue'}>评分</Tag>
                                     <BiliBiliScoreTag
                                         score={item.bilibili_score}
                                         user_count={item.bilibili_user_count}
                                         style={{fontSize:'0.7rem',padding:'0.1rem',width:'2.7rem',height:'1.2rem'}}
                                         logoStyle={{width:'.8rem'}}
                                     />
+                                    <BangumiScoreTag
+                                        score={item.score}
+                                        user_count={item.vote_count}
+                                        style={{fontSize:'0.7rem',padding:'0.1rem',width:'2.7rem',height:'1.2rem'}}
+                                        logoStyle={{width:'.8rem'}}
+                                    />
                                 </div>
-                                <div className={'item-info-tag'}><Tag color={'geekblue'}>总话数:</Tag>{item.episode_count || '暂无'}</div>
-                                <div className={'item-info-tag'}><Tag color={'geekblue'}>放送日期:</Tag>{item.start_date || '暂无'}</div>
-                                <div className={'item-info-tag'}><Tag color={'geekblue'}>简介:</Tag>
+                                <div className={'item-info-tag'}><Tag color={'geekblue'}>放送日期</Tag>{item.start_date || '暂无'}</div>
+                                <div className={'item-info-tag'}><Tag color={'geekblue'}>简介</Tag>
                                     <div id={'description-wrapper'} dangerouslySetInnerHTML={item.description}>
                                     </div>
                                 </div>
