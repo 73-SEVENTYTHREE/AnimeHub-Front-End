653ba4a2be00659c372f109812fe2db02018e7e2
73-SEVENTYTHREE <2569535507@qq.com>
2021-07-20 11:48:44 +0800

    更新书籍详情页

--- src/components/BookInfo/index.jsx
+++ src/components/BookInfo/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {Card, Col, Divider, Row, Image, Tag, Tabs, Typography, List, Avatar} from "antd";
+import {Card, Col, Divider, Row, Image, Tag, Tabs, Typography, List, Avatar, Empty} from "antd";
 import {Link} from "react-router-dom";
 import KnowledgeGraph from "../KnowledgeGraph";
 import Tags from "../Tags";
 import InfoTimeline from "../InfoTimeline";
 import removeLastCharacter from "../../utils/removeLastCharacter";
+import Meta from "antd/es/card/Meta";
 
 const {TabPane} = Tabs
 
@@ -14,15 +15,15 @@ function BookInfo(props) {
     return (
         <div>
             <div id={'result-container-bg'} style={{ background:`url("${removeLastCharacter(data.visuals)}")`}}/>
-        <Card style={{margin:'1rem 2rem 2rem 2rem', minHeight:'30rem'}} hoverable>
+        <Card style={{margin:'2rem 2rem 2rem 2rem', minHeight:'30rem'}} hoverable>
             <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
                 <Col span={mobile ? 24 : 6}>
                     <div style={{display:'flex', flexDirection:'column'}}>
-                        <div style={{margin:'0 auto'}}><Image width={'10rem'} src={data.visuals[0]}/></div>
+                        <div style={{margin:'0 auto'}}><Image width={'10rem'} src={removeLastCharacter(data.visuals)}/></div>
                         <div style={{display:'flex', flexDirection:'column'}}>
                             <Divider orientation={'left'}>名称</Divider>
-                            <div >
-                                <Tag>原名</Tag>{data.primary_name}<br/>
+                            <div>
+                                <Tag style={{marginBottom:'.5rem'}}>原名</Tag>{data.primary_name}<br/>
                                 <Tag>中文名</Tag>{data.zh_name}
                             </div>
                             <Divider orientation={'left'}>作者</Divider>
@@ -50,7 +51,15 @@ function BookInfo(props) {
                 </Col>
                 <Divider type={mobile ? "horizontal": "vertical"} style={mobile? {}:{height:'100%'}}/>
                 <Col span={mobile ? 24:17}>
-                    <Tabs defaultActiveKey="1">
+                    <Tabs defaultActiveKey="1" type={'card'} onChange={(key) => {
+                        setTimeout(() => {
+                            const relevantContainer = document.getElementById('relevant-container');
+                            let resultContainer = document.getElementById('result-container');
+                            relevantContainer.style.top = resultContainer.offsetHeight + 'px'
+                            const container = document.getElementById('detail-container');
+                            container.style.height = document.body.scrollHeight.toString() + 'px';
+                        }, 200)
+                    }}>
                         <TabPane key={'1'} tab={"书籍简介"}>
                             <div style={{display:'flex',flexDirection:'column',padding:'1rem'}}>
                                 <Typography.Title level={5} color={'blue'}>内容简介</Typography.Title>
@@ -58,30 +67,36 @@ function BookInfo(props) {
                                 <Typography.Title level={5}>大家倾向于把{data.primary_name}标记为：</Typography.Title>
                                 <div>
                                     {
-                                        <Tags tags={data.tags}/>
+                                        <Tags tags={data.tags} history={props.history}/>
                                     }
                                 </div>
                             </div>
                         </TabPane>
                         <TabPane key="2" tab={"人物介绍"}>
-                            <List
-                                itemLayout="horizontal"
-                                dataSource={data.chara_list}
-                                renderItem={item => (
-                                    <List.Item>
-                                        <List.Item.Meta
-                                            avatar={<Avatar src={item.visuals} />}
-                                            title={<a href="https://ant.design">{item.primary_name}</a>}
-                                            description={
-                                                <div>
+                            <div style={{marginTop:'2rem', display:'flex', justifyContent:'center', alignItems:'center', flexWrap:'wrap'}}>
+                            {
+                                data.chara_list.map(item =>
+                                    (
+                                        <div style={{width:'70%'}}>
+                                            <Meta
+                                                avatar={
+                                                    <Avatar src={item.visuals} draggable/>
+                                                }
+                                                style={{minWidth:'15rem', marginRight:'2rem'}}
+                                                title={<Link to={{pathname:'result', state:{searchString:item.primary_name}}}>{item.primary_name}</Link>}
+                                                description={<div>
                                                     <Tag>中文名：{item.zh_name}</Tag>
-                                                    <Tag>{item.role}</Tag>
-                                                </div>
-                                            }
-                                        />
-                                    </List.Item>
-                                )}
-                            />
+                                                    <Divider style={{padding:'0', margin:'1rem'}}/>
+                                                </div>}
+                                            />
+                                        </div>
+                                    )
+                                )
+                            }
+                                {
+                                    data.chara_list.length === 0 ? <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description={'暂无人物介绍'}/>:''
+                                }
+                            </div>
                         </TabPane>
                     </Tabs>
                 </Col>
